f7b369443c6cb79c93222988e052b28fc9f8abda
HUNGDT <hk2t.thaihung@gmail.com>
2019-01-14 10:09:39 +0700

    done

---
+++ src/main/java/com/hk3t/model/MyCounter.java
@@ -0,0 +1,24 @@
+package com.hk3t.model;
+
+public class MyCounter {
+    private int count;
+
+    public MyCounter() {
+    }
+
+    public MyCounter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int inrement() {
+        return count++;
+    }
+}
